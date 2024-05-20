c0ff01542567b9e97e37eb97744d82d7418ac332
ARamos <albertramos902@gmail.com>
2024-05-20 22:05:23 +0100

    very advance ls command with lots of information

--- 8.ls-advanced.js
+++ 8.ls-advanced.js
@@ -1,18 +1,54 @@
 const fs = require("node:fs/promises");
-
+const path = require("node:path")
 
 //here we can pass an argument when calling this function, if the argument dosnt exists as directory it uses the default "."
 const folder = process.argv[2] ?? "."
 
-fs.readdir(folder)
-  .then(files => {
-    files.forEach(file => {
+//using the folder we create the ls
+async function ls(folder) {
+  let files
+  try {
+    //we read the files
+    files = await fs.readdir(folder)
+  }
+  catch {
+    //catch the error and exit. important!
+    console.error(`Error when loading the directory ${folder}`)
+    process.exit(1)
+  }
+
+
+  const filePromises = files.map(async file => {
+    //for each fole we get the path
+    const filePath = path.join(folder, file)
+    let fileStats
+    //we get the filepath from the stat
+    try {
+       fileStats = await fs.stat(filePath)
+    }
+    //catch error and exit, important!
+    catch {
+      console.error("Cannot read files")
+      process.exit(1)
+    }
+
+    //true or false
+    const isDirectory = fileStats.isDirectory();
+    //one symbol or another
+    const fileType = isDirectory ? "d" : "-"
+    //the file size
+    const fileSize = fileStats.size;
+    //last time modified
+    const fileModified = fileStats.mtime.toLocaleString();
+
+    //we return a template string with all the information
+    return `${fileType} ${file} ${fileSize.toString()} ${fileModified}`
+  })
+  const filesInfo = await Promise.all(filePromises)
+  filesInfo.forEach(file => {
     console.log(file)
   })
-  })
-  .catch(err => {
-    if (err) {
-      console.error('Error when loading the directory', error)
-      return
-  }
-})
+}
+
+ls(folder)
+
