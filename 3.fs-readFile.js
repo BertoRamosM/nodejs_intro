10c2cedcc4f7646d58ce83c1fd7df0f3cf01663b
ARamos <albertramos902@gmail.com>
2024-05-20 19:02:37 +0100

    reading files in parallel lesson

--- 3.fs-readFile.js
+++ 3.fs-readFile.js
@@ -1,5 +1,9 @@
-const fs = require('node:fs')
 
+//with 'promisify' we can create promises when the native module that its not a callback but we need a promise
+//const {promisify} = require('node:util')
+//const readFilePromise = promisify(fs.readFile)
+
+const fs = require("node:fs");
 console.log('reading first file')
 fs.readFile('./file.txt', 'utf-8', (err, data) => {
   console.log(data)
---
+++ 4.fs.async-await-parallel.mjs
@@ -0,0 +1,12 @@
+import { readFile } from 'node:fs/promises'
+
+//this is in parallel so much faster as we read two files at the same time, and keeps running the script once both files have been readed
+
+Promise.all([
+  readFile('./file.txt', 'utf-8'),
+    readFile('./file2.txt', 'utf-8')
+]).then(([text, secondText]) => {
+  console.log('first text', text)
+  console.log("second text", secondText);
+})
+
