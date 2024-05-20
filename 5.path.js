90e6fb02eb87c53f0076ff0a14785ca8b43bffb9
ARamos <albertramos902@gmail.com>
2024-05-20 19:19:42 +0100

    working with paths lesson

---
+++ 5.path.js
@@ -0,0 +1,23 @@
+const path = require('node:path')
+
+//unix the slash its /
+//windows the slash its \
+
+//this will tell us the slash of our OS
+console.log(path.sep)
+
+//thats why we have to put together routes with path.join() and not manually
+const filePath = path.join('content', 'subfolder', 'file.txt')
+console.log(filePath)
+
+//with "base" we can get the file name from a full path
+const base = path.basename('/tmp/albert-secret-files/password.txt')
+console.log(base)
+
+//we can also get only the file name without his extension (.txt)
+const fileName = path.basename('/tmp/albert-secret-files/password.txt', '.txt')
+console.log(fileName)
+
+//we can also get only the extension
+const extension = path.extname('image.jpg')
+console.log(extension)
